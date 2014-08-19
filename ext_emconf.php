ea040b1ba71cf670a4332a69316897bb8140efcc
Danet Krueng <danet@web-essentials.asia>
2014-08-19 14:28:00 +0700

    [TASK] Initalize extension difference_viewer

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "difference_viewer".
+#
+# Auto generated 06-07-2011 07:59
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Difference viewer',
+	'description' => 'Difference viewer for BE data aggregator',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '2.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Dmitry Gordienko',
+	'author_email' => 'dmitry.gordienko@gmail.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:13:{s:9:"CHANGELOG";s:4:"6ef1";s:10:"README.txt";s:4:"ee2d";s:9:"close.gif";s:4:"9905";s:12:"ext_icon.gif";s:4:"b328";s:17:"ext_localconf.php";s:4:"f987";s:14:"ext_tables.php";s:4:"a757";s:16:"locallang_db.xml";s:4:"3066";s:14:"doc/manual.sxw";s:4:"7e6e";s:19:"doc/wizard_form.dat";s:4:"0862";s:20:"doc/wizard_form.html";s:4:"7f42";s:37:"pi1/class.tx_differenceviewer_pi1.php";s:4:"fff5";s:17:"pi1/locallang.xml";s:4:"4922";s:24:"pi1/static/editorcfg.txt";s:4:"8206";}',
+);
+
+?>
